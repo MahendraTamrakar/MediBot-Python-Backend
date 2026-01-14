f7010d91a14a7727bee2a3bae4fbc3e7a4216538
MahendraTamrakar <tamrakarmahendra157@gmail.com>
2026-01-15 00:34:50 +0530

    user_details updated

--- db/user_details.py
+++ db/user_details.py
@@ -4,12 +4,14 @@ from datetime import date
 
 class PersonalDetails(BaseModel):
     email: EmailStr
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-    dob: Optional[date] = None
+    full_name: Optional[str] = None
+    age: Optional[int] = None
     gender: Optional[str] = None
-    blood_group: Optional[str] = None
+    height: Optional[float] = None
+    weight: Optional[float] = None
+    blood_type: Optional[str] = None
     phone: Optional[str] = None
+    address: Optional[str] = None
     profile_photo_url: Optional[str] = None
 
 class SavePersonalDetailsRequest(BaseModel):
